3e3cb455ea485bdb9e3a598ae6169d2df988eabf
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-06 17:29:25 +0000

    Modified CSS

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
   // const h1 = document.querySelector('h1')
   // h1.textContent = 'Hello World!'
 
 
-$(document).ready(function() {
+$().ready(function() {
 
-  $('.little-grid').find("td:not(.filled)").click(function() {
-    if($(this).closest('.play').length === 0) {
+  $(".grid").find("td:not(.filled)").click(function() {
+    if($(this).closest('.live').length === 0) {
       return;
     }
 // assign choice blue or red
-  const colour =    $('#turn').hasClass('blue') ? 'blue' : 'red'
+  const colour =    $('#turn').hasClass('red') ? 'red' : 'blue'
 
 // declaring objects
-  smallgrid    =    $(this).closest('.little-grid');
-  smallcolumn  =    $(this).attr('class');
-  smallrow     =    $(this).closest('tr').attr('class');
+  little_table    =    $(this).closest('.little-grid');
+  little_col  =    $(this).attr('class');
+  little_row     =    $(this).closest('tr').attr('class');
 
 // giving value of single play
-  played = '.col-' + smallcolumn + '.row-' + smallrow;
+  little_pos = '.col-' + litte_col + '.row-' + little_row;
 
 // decide player move on small grid
-  $(".play").removeClass('play');
-  if($('.smallgrid' + played + ' td').not('.filled').length === 0) {
-    $('.big-grid').addClass('play');
+  $(".live").removeClass('live');
+  if($('.little_table' + little_pos + ' td').not('.filled').length === 0) {
+    $('.big').addClass('live');
   } else {
-    $(".smallgrid" + played).addClass('play');
+    $(".little_table" + little_pos).addClass('play');
   }
 
 
 // prevent to play again in the same grid
-  let div = $('<div>').addClass('score').addClass(colour).addClass('col-' + smallcolumn).addClass('row-' + smallrow)
+  let div = $('<div>').addClass('marker').addClass(colour).addClass('col-' + smallcolumn).addClass('row-' + smallrow)
   $(this).html(score);
   $(this).addClass('filled')
   $(this).unbind('click')
 
-  score = smallgrid.find('score').filter('.' + colour)
+  score = smallgrid.find('.score').filter('.' + colour)
 
-// declaring how is the winner on small grid
+// declaring who is the winner on small grid
 if(vertical(score) || horizontal(score) || diagonal(score)) {
 smallgrid.addClass(colour)
 smallgrid.find('td').css('backgroundColor', 'blue')
@@ -45,39 +45,46 @@ smallgrid.find('td').css('backgroundColor', 'blue')
   smallgrid.find('td').css('backgroundColor', 'red')
 }
 
+// declaring same if for big-grid
 score = $('.smallgrid').filter('.' + colour)
 
 if(vertical(score) || horizontal(score) || diagonal(score)) {
   win();
-} else if {
-  ($('td').not('.filled').legth === 0)
-    newGame();
+} else {
+  ($('td').not('.filled').length === 0) {
+    $('#turn').fadeOut(500, function() {
+      newGame();
+    }
   } else {
     $('#turn').removeClass(colour).addClass((colour === 'red') ? 'blue' : 'red');
     $('turn').html((colour === 'red') ? 'blue' : 'red')
   }
   }
-}
-}
+});
 
 function winner () {
   const left = $('.big-grid').offset().left
   const middle = $('.big-grid').position().middle
   const right = $('.#turn').offset().right
+});
+
+$('#turn').text('');
+
+$('#turn').css ({
+  "position": "absolute",
+  "left": "left" + "px",
+  "right": "right" + "px"
+  newGame()
+})
+};
+
+function vertical(score) {
+  return (($(score).filter('.row-1').length === 3)) || ($(score).filter('.row-2').length === 3) || (($(score).filter('.row-3').length === 3))
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+function horizontal(score) {
+  return (($(score).filter('.col-1').length === 3)) || ($(score).filter('.col-2').length === 3) || (($(score).filter('.col-3').length === 3))
+}
 
 
 
