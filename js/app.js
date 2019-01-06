247962dec669fe5700c3b7b1f161da4d076a9513
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-06 10:22:23 +0000

    Assigned value to single token on play

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
   // const h1 = document.querySelector('h1')
   // h1.textContent = 'Hello World!'
+$(document).ready(function() {
+
+  $('.little-grid').find("td:not(.filled)").click(function() {
+    if($(this).closest('.live').length === 0) {
+      return;
+    }
+// assign choice blue or red
+  const colour =    $('#turn').hasClass('blue') ? 'blue' : 'red'
+
+// declaring objects
+  smallgrid   =    $(this).closest('.little-grid');
+  smallcolumn =    $(this).attr('class');
+  smallrow    =    $(this).closest('tr').attr('class');
+
+// giving value of single play
+  position = '.col-' + smallcolumn + '.row-' + smallrow;
+
+  
+
+
+
+
+
+
+
+
+
+
 })
