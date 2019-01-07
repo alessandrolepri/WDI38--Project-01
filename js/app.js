ab04065cbc75c8dca5d668037c356b70f3640358
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-07 09:32:52 +0000

    Fixed issue JS

--- js/app.js
+++ js/app.js
@@ -1,107 +1,105 @@
 // document.addEventListener('DOMContentLoaded', () => {
-  // const h1 = document.querySelector('h1')
-  // h1.textContent = 'Hello World!'
+// const h1 = document.querySelector('h1')
+// h1.textContent = 'Hello World!'
 
 
-  $(document).ready(function() {
+$().ready(function() {
 
-    $(".grid").find("td:not(.filled)").click(function() {
-      if($(this).closest('.live').length === 0) {
-        return;
-      }
-      // assign choice blue or red
-      let colour = $('#turn').hasClass('red') ? 'red' : 'blue'
+  $('.grid').find('td:not(.filled)').click(function() {
+    if($(this).closest('.live').length === 0) {
+      return
+    }
+    // assign choice blue or red
+    const colour = $('#turn').hasClass('red') ? 'red' : 'blue'
 
-      // declaring objects
-      little_table  =    $(this).closest('.little-grid');
-      little_col    =    $(this).attr('class');
-      little_row    =    $(this).closest('tr').attr('class');
+    // declaring objects
+    const littleTable  =    $(this).closest('.little-grid')
+    const littleCol    =    $(this).attr('class')
+    const littleRow    =    $(this).closest('tr').attr('class')
 
-      // giving value of single play
-      little_pos = '.col-' + little_col + '.row-' + little_row;
+    // giving value of single play
+    const littlePos = '.col-' + littleCol + '.row-' + littleRow
 
-      // decide player-next move on small grid
-      $(".live").removeClass('live');
-      if($('.grid' + little_pos + ' td').not('.filled').length === 0) {
-        $('.big').addClass('live');
+    // decide player-next move on small grid
+    $('.live').removeClass('live')
+    if($('.grid' + littlePos + ' td').not('.filled').length === 0) {
+      $('.big').addClass('live')
+    } else {
+      $('.grid' + littlePos).addClass('live')
+    }
+
+    // prevent to play again in the same grid
+    const div = $('<div>').addClass('marker').addClass(colour).addClass('col-' + littleCol).addClass('row-' + littleRow)
+    $(this).html(div)
+    $(this).addClass('filled')
+    $(this).unbind('click')
+
+    let markers = littleTable.find('.marker').filter('.' + colour)
+
+    // declaring who is the winner on small grid
+    if(vertical(markers) || horizontal(markers) || diagonal(markers)) {
+      littleTable.addClass(colour)
+      if(colour === 'red') {
+        littleTable.find('td').css('backgroundColor', 'blue')
       } else {
-        $(".grid" + little_pos).addClass('live');
+        littleTable.find('td').css('backgroundColor', 'red')
       }
+    }
 
-      // prevent to play again in the same grid
-      let div = $('<div>').addClass('marker').addClass(colour).addClass('col-' + little_col).addClass('row-' + little_row);
-      $(this).html(div);
-      $(this).addClass('filled');
-      $(this).unbind('click');
+    // declaring same if for big-grid
+    markers = $('.little_table').filter('.' + colour)
 
-      markers = little_table.find('.marker').filter('.' + colour);
+    function newGame() {
+      $('#newGame').css('display', 'block').hide().fadeIn(900)
+    }
 
-      // declaring who is the winner on small grid
-      if(vertical(markers) || horizontal(markers) || diagonal(markers)) {
-        little_table.addClass(colour);
-        if(colour === 'red') {
-          little_table.find('td').css('backgroundColor', 'blue');
-        } else {
-          little_table.find('td').css('backgroundColor', 'red');
-        }
-      }
-
-      // declaring same if for big-grid
-      markers = $('.little_table').filter('.' + colour);
-
-      if(vertical(markers) || horizontal(markers) || diagonal(markers)) {
-        win();
+    if(vertical(markers) || horizontal(markers) || diagonal(markers)) {
+      winner()
+    } else {
+      if($('td').not('.filled').length === 0) {
+        $('#turn').fadeOut(900, function() {
+          newGame()
+        })
       } else {
-        if($('td').not('.filled').length === 0) {
-          $('#turn').fadeOut(200, function() {
-            newGame();
-          });
-        } else {
-          $('#turn').removeClass(colour).addClass(colour === 'red' ? 'blue' : 'red');
-          $('#turn').html((colour === 'red' ? 'blue' : 'red') + '&rsquo;s turn&hellip;');
-        }
+        $('#turn').removeClass(colour).addClass(colour === 'red' ? 'blue' : 'red')
+        $('#turn').html((colour === 'red' ? 'blue' : 'red') + '&rsquo;s turn&hellip;')
       }
-    });
-  });
+    }
+  })
+})
 
-  function winner() {
-    let right = $('#turn').offset().top;
-    let top = $('.big').position().top;
-    let left = $('.big').offset().left;
+function winner() {
+  const right = $('#turn').offset().top
+  const top = $('.big').position().top
+  const left = $('.big').offset().left
 
-    $('#turn').text('');
+  $('#turn').text('You Won!!!')
 
-    $('#turn').css ({
-      position: 'absolute',
-      left: left + 'px',
-      top: right + 'px'
-    });
-  }
+  $('#turn').css({
+    position: 'absolute',
+    left: left + 'px',
+    top: right + 'px'
+  })
 
-  function newGame() {
-    $('#newGame').css('display', 'block').hide().fadeIn(200);
-  }
 
   function horizontal(markers) {
     return (
       $(markers).filter('.row-1').length === 3 || $(markers).filter('.row-2').length === 3 || $(markers).filter('.row-3').length === 3
-    );
+    )
   }
   function vertical(markers) {
     return (
       $(markers).filter('.col-1').length === 3 || $(markers).filter('.col-2').length === 3 || $(markers).filter('.col-3').length === 3
-    );
+    )
   }
 
   function diagonal(markers) {
     if($(markers).filter('col-2').filter('.row-2').length === 0)
-    {
-      return false;
-    }
-    else if($(markers).filter('.col-1').filter('.row-1').length === 1 && $(markers).filter('.col-3').filter('.row-1').length === 1)
-    {
-      return true;
-    } else {
-      return false;
-    }
+      return false
   }
+  if($(markers).filter('.col-1').filter('.row-1').length === 1 && $(markers).filter('.col-3').filter('.row-1').length === 1) {
+    return true
+  } else {
+    return false
+  }
+}
